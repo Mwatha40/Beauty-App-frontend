5dd6fa16488e73fd9428a3a1a09f44b18914050d
wambui11nderitu <nderituwambui93@gmail.com>
2024-09-02 14:39:46 +0300

    updating

--- beauty-app/src/components/CartItem.js
+++ beauty-app/src/components/CartItem.js
@@ -1,16 +1,17 @@
 import React from 'react';
 
-
 const CartItem = ({ item, removeFromCart }) => {
   return (
     <div className="cart-item">
-      {/* <h4>{item.name}</h4>
+      <h4>{item.name}</h4>
       <p>Price: ${item.price}</p>
       <p>Quantity: {item.quantity}</p>
-      <button onClick={() => removeFromCart(item.id)}>Remove</button> */}
-      <h1>this route is working</h1>
+      <button onClick={() => removeFromCart(item.id)}>Remove</button>
+      <h1>This route is working</h1>
     </div>
   );
 };
 
 export default CartItem;
+
+
